b73e307ee1bff615f8c0df6265b068c6eedc7149
Mirty1337 <mirty1337@gmail.com>
2022-02-13 18:04:15 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    token: "OTM5NDI5MDk0MjMwMjIwODIw.Yf4thQ.-EQDaCG9SQX_STwMQmUo0eOqDTs",
+    prefix: '!',
+    database: {
+        username: 'root',
+        password: '',
+        name: 'test',
+    },
+    owners: ["405958679842783243", "405958679842783243", "405958679842783243"]
+}
