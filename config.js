93d5912cc359dc8f5f6a7e95733a278bcb09aa19
Mirty1337 <mirty1337@gmail.com>
2022-02-13 18:07:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    token: "OTM5NDI5MDk0MjMwMjIwODIw.Yf4thQ.-EQDaCG9SQX_STwMQmUo0eOqDTs",
+    token: "",
     prefix: '!',
     database: {
         username: 'root',
         password: '',
         name: 'test',
     },
-    owners: ["405958679842783243", "405958679842783243", "405958679842783243"]
+    owners: ["235115589616664576"]
 }
